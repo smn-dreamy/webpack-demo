f88604511ebeb07e834cc2ca5efabdbc1e30cca4
unknown <18234083378@163.com>
2020-05-15 18:29:30 +0800

    。

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports={
+  mode:"development",
+  entry:{
+    main:'./src/index.js'
+  },
+  module:{
+    rules:[
+      {
+        test:/\.png$/,
+        use:{
+          loader: 'file-loader'
+        }
+      }
+    ]
+  },
+  output:{
+    filename:'bundle.js',
+    path: path.resolve(__dirname,'build')
+  }
+}

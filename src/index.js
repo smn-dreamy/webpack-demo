ecd8f7d167dbdb6a6faf5c2ba65ae3c62af07135
unknown <18234083378@163.com>
2020-05-18 19:23:44 +0800

    。

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import avatar from './logo.png';
+
+var img = new Image();
+img.src = avatar;
+
+var root = document.getElementById('root');
+root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 const path = require('path');
-module.exports={
-  mode:"development",
-  entry:{
-    main:'./src/index.js'
-  },
-  module:{
-    rules:[
-      {
-        test:/\.png$/,
-        use:{
-          loader: 'file-loader'
-        }
-      }
-    ]
-  },
-  output:{
-    filename:'bundle.js',
-    path: path.resolve(__dirname,'build')
-  }
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js'
+	},
+	module: {
+		rules: [{
+			test: /\.png$/,
+			use: {
+				loader: 'file-loader'
+			}
+		}]
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
 }

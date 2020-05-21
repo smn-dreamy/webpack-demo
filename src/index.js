c88a01ab83c2240ef62d80b47dac8e76be1ede47
unknown <18234083378@163.com>
2020-05-21 10:34:39 +0800

    react config

--- src/index.js
+++ src/index.js
@@ -35,20 +35,30 @@
 //   })
 // }
 
-// import "@babel/polyfill";
+import "@babel/polyfill";
 
-const arr=[
-  new Promise(()=>{}),
-  new Promise(()=>{})
-]
+// const arr=[
+//   new Promise(()=>{}),
+//   new Promise(()=>{})
+// ]
+//
+// arr.map(item=>{
+//   console.log(item)
+// })
+//
 
-arr.map(item=>{
-  console.log(item)
-})
 
 
+import React,{Component} from 'react'
+import ReactDom from 'react-dom';
 
+class App extends Component{
+    render(){
+      return <div>hello world</div>
+    }
+}
 
+ReactDom.render(<App/>,document.getElementById('root'))
 
 
 

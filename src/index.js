2ef5a21a79ce619daca76718290c0714e5df7c02
unknown <18234083378@163.com>
2020-06-16 18:51:47 +0800

    pwa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('hello,this is huahua')
+
+if('serviceWorker' in navigator){
+  window.addEventListener('load',()=>{
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(registration=>{
+      console.log('service-worker registed')
+    }).catch(err=>{
+      console.log('service-worker register error')
+    })
+  })
+}
